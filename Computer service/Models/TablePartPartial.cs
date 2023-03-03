ff53b70da71bf1a7fb6d39fd79afad90d7474365
LeGGioN-ru <nikita90017@gmail.com>
2023-03-03 19:22:13 +0300

    Add print. Change design in authorization and check.

--- Computer service/Models/TablePartPartial.cs
+++ Computer service/Models/TablePartPartial.cs
@@ -6,5 +6,25 @@ namespace Computer_service.Models
     {
         public int CountServices => TB_Services.Count;
         public int SumServices => TB_Services.Sum(x => x.Service.Service_price);
+
+        public override string ToString()
+        {
+            string finalString = string.Empty;
+
+            finalString += $"№Номер чека {Entry_number}\n";
+            finalString += $"Услуги:\n";
+            finalString += "_________\n";
+
+            foreach (TB_Services tb_service in TB_Services)
+                finalString += tb_service.ToString() + "\n";
+
+            finalString += "_________\n";
+            finalString += $"Количество услуг: {CountServices}\n";
+            finalString += $"Сумма чека: {SumServices}\n";
+            finalString += $"ИНН 1234567899\n";
+            finalString += $"Адрес: Г.Серпухов ул. Ленина 110";
+
+            return finalString;
+        }
     }
 }
